51032885cb4f75f582867d31324341127593374b
Terrasean123 <106635112+Terrasean123@users.noreply.github.com>
2023-03-17 16:01:17 +1100

    Color Class Created

---
+++ MathsClasses/MathsClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Colour color  = new Colour(0,255,255,255);
+            Console.WriteLine(color.colour);
+            Console.WriteLine(Colour.ConvertToBitString(color.colour));
+         
+        }
+    }
+}
