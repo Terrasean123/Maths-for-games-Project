7f89d04a20f90631600fddec8e407b921926a14d
Terrasean123 <106635112+Terrasean123@users.noreply.github.com>
2023-04-06 17:03:14 +1000

    bullet movement being worked on

--- Task3/AIEYear1Samples-master/RaylibStarterCS/Game.cs
+++ Task3/AIEYear1Samples-master/RaylibStarterCS/Game.cs
@@ -21,10 +21,13 @@ namespace RaylibStarterCS
         SceneObject tankObject = new SceneObject();
         SceneObject turretObject = new SceneObject();
 
+        SceneObject bulletObject = new SceneObject();
+        SpriteObject bulletSprite = new SpriteObject();
+
         SpriteObject tankSprite = new SpriteObject();
         SpriteObject turretSprite = new SpriteObject();
 
-
+         
         Camera2D camera;
 
         public Game()
@@ -52,7 +55,8 @@ namespace RaylibStarterCS
 
             turretSprite.Load("L:/Maths for games project/Maths-for-games-Project/Task3/AIEYear1Samples-master/Sprites/Tank turret.png");
             turretSprite.SetRotate(-90 * (float)(Math.PI / 180.0f));
-            turretSprite.SetPosition(-70, turretSprite.Width / 2.0f);
+            turretSprite.SetPosition(-60, turretSprite.Width / 2.0f);
+            turretObject.Translate(-15, 0);
 
             // set up the scene object hierarchy - parent the turret to the base,
             // then the base to the tank sceneObject
@@ -63,9 +67,11 @@ namespace RaylibStarterCS
             // having an empty object for the tank parent means we can set the
             // position/rotation of the tank without
             // affecting the offset of the base sprite
-            tankObject.SetPosition(GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f);
-            SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
 
+            tankObject.SetPosition(GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f);
+            SetTargetFPS(60);
+
+            // Set our game to run at 60 frames-per-second
             //camera.target = new Vector2(0, 0);
             //camera.offset = Vector2.Zero;
             //camera.rotation = 0;
@@ -74,6 +80,8 @@ namespace RaylibStarterCS
 
         public void Shutdown()
         {
+
+
         }
 
         public void Update()
@@ -92,39 +100,52 @@ namespace RaylibStarterCS
             //m_timer += deltaTime;
 
             // insert game logic here
-            if (IsKeyDown(KeyboardKey.KEY_RIGHT))
+            
+            if (IsKeyDown(KeyboardKey.KEY_SPACE))
+            {
+                //SceneObject bulletObject = new SceneObject();
+                //SpriteObject bulletSprite = new SpriteObject();
+                bulletSprite.Load("L:/Maths for games project/Maths-for-games-Project/Task3/AIEYear1Samples-master/Sprites/bulletDark1_outline.png");
+                bulletObject.AddChild(bulletSprite);
+                //bulletObject.Draw();
+                MathClasses.Vector3 facing = new MathClasses.Vector3(tankSprite.LocalTransform.m00,
+                   tankObject.LocalTransform.m01, 1); 
+                bulletObject.SetPosition(facing.x,facing.y);
+            }
+
+            if (IsKeyDown(KeyboardKey.KEY_LEFT))
             {
 
                 turretObject.Rotate(-deltaTime);
 
             }
 
-            if (IsKeyDown(KeyboardKey.KEY_LEFT))
+            if (IsKeyDown(KeyboardKey.KEY_RIGHT))
             {
 
                 turretObject.Rotate(deltaTime);
             }
 
-            if (IsKeyDown(KeyboardKey.KEY_A)) 
+            if (IsKeyDown(KeyboardKey.KEY_A))
             {
 
                 tankObject.Rotate(-deltaTime);
-            
+
             }
 
-            if (IsKeyDown(KeyboardKey.KEY_D)) 
+            if (IsKeyDown(KeyboardKey.KEY_D))
             {
 
                 tankObject.Rotate(deltaTime);
             }
 
-            if(IsKeyDown(KeyboardKey.KEY_W)) 
+            if (IsKeyDown(KeyboardKey.KEY_W))
             {
                 MathClasses.Vector3 facing = new MathClasses.Vector3(tankObject.LocalTransform.m00,
                     tankObject.LocalTransform.m01, 1) * deltaTime * -100;
-               tankObject.Translate(facing.x, facing.y);
-            
-            
+                tankObject.Translate(facing.x, facing.y);
+
+
             }
 
             if (IsKeyDown(KeyboardKey.KEY_S))
@@ -136,6 +157,7 @@ namespace RaylibStarterCS
 
             }
             tankObject.Update(deltaTime);
+
         }
 
         public void Draw()
@@ -144,6 +166,7 @@ namespace RaylibStarterCS
 
             ClearBackground(Color.WHITE);
             DrawText(fps.ToString(), 10, 10, 14, Color.RED);
+            bulletObject.Draw();
             tankObject.Draw();
             EndDrawing();
         }
